30b9964c925e6d5232bf316b4b146c2d1ff6d7df
Inomag <pratiksr12345@gmail.com>
2021-08-28 13:39:59 +0530

    Initial

---
+++ src/main/java/com/demo/security/config/DemoAppConfig.java
@@ -0,0 +1,30 @@
+package com.demo.security.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages="com.demo.security")
+public class DemoAppConfig {
+	
+	// DEFINE A BEAN FOR VIEWRESOLVER
+	
+	@Bean
+	public ViewResolver viewResolver() {
+		
+		InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+		
+		viewResolver.setPrefix("/WEB-INF/view/");
+		viewResolver.setSuffix(".jsp");
+		
+		
+		return viewResolver;
+		
+	}
+
+}
---
+++ src/main/java/com/demo/security/config/DisSerInit.java
@@ -0,0 +1,22 @@
+package com.demo.security.config;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class DisSerInit extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+	@Override
+	protected Class<?>[] getRootConfigClasses() {		
+		return null;
+	}
+
+	@Override
+	protected Class<?>[] getServletConfigClasses() {		
+		return new Class[] {DemoAppConfig.class};
+	}
+
+	@Override
+	protected String[] getServletMappings() {
+		return new String[] {"/"};
+	}
+
+}
