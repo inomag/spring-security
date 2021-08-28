e0bb1930ea1bcd49b620155ab80dc80441084a74
Inomag <pratiksr12345@gmail.com>
2021-08-28 14:06:48 +0530

    Security Config

---
+++ src/main/java/com/demo/security/config/DemoSecurityConfig.java
@@ -0,0 +1,26 @@
+package com.demo.security.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.User.UserBuilder;
+
+@Configuration
+@EnableWebSecurity
+public class DemoSecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		// ADD USERS
+		UserBuilder users = User.withDefaultPasswordEncoder();
+		
+		auth.inMemoryAuthentication()
+		.withUser(users.username("john").password("test123").roles("EMPLOYEE"))
+		.withUser(users.username("mary").password("test123").roles("MANAGER"))
+		.withUser(users.username("susan").password("test123").roles("ADMIN"));
+	}
+
+	
+}
---
+++ src/main/java/com/demo/security/config/SecurityWebAppInit.java
@@ -0,0 +1,7 @@
+package com.demo.security.config;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityWebAppInit extends AbstractSecurityWebApplicationInitializer {
+
+}
