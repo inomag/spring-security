c00bdb504082e92e5f349546bfffcd3ea147c9a5
Inomag <pratiksr12345@gmail.com>
2021-09-04 11:26:20 +0530

    JDBC integration

--- src/main/java/com/demo/security/config/DemoAppConfig.java
+++ src/main/java/com/demo/security/config/DemoAppConfig.java
@@ -1,30 +1,87 @@
 package com.demo.security.config;
 
+import java.beans.PropertyVetoException;
+import java.util.logging.Logger;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
+import com.mchange.v2.c3p0.ComboPooledDataSource;
+
 @Configuration
 @EnableWebMvc
 @ComponentScan(basePackages="com.demo.security")
+@PropertySource("classpath:persistence-mysql.properties")
 public class DemoAppConfig {
 	
-	// DEFINE A BEAN FOR VIEWRESOLVER
 	
+	// SETUP VARIABLE TO HOLD PROPERTIES
+	@Autowired
+	private Environment env;
+	
+	// SETUP LOGGER FOR DIAGNOSTICS
+	private Logger logger = Logger.getLogger(getClass().getName());
+	
+	
+	// DEFINE A BEAN FOR VIEWRESOLVER
 	@Bean
 	public ViewResolver viewResolver() {
-		
 		InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
-		
 		viewResolver.setPrefix("/WEB-INF/view/");
 		viewResolver.setSuffix(".jsp");
-		
-		
 		return viewResolver;
+	}
+	
+	// DEFINE A BEAN FOR OUR SECURITY DATASOURCE
+	
+	@Bean
+	public DataSource securityDataSource() {
+		// CREATE CONNECTION POOL
+		ComboPooledDataSource securitySource
+			= new ComboPooledDataSource();
 		
+		// SET JDBC DRIVER
+		try {
+			securitySource.setDriverClass(env.getProperty("jdbc.driver"));
+		} catch (PropertyVetoException e) {
+			throw new RuntimeException(e);
+		}
+		
+		// LOG CONNECTION PROPS
+		logger.info(">>>>> jdbc.url = "+env.getProperty("jdbc.url"));
+		logger.info(">>>>> jdbc.user = "+env.getProperty("jdbc.user"));
+		
+		
+		// SET DATABASE CONNECTION PROPS
+		securitySource.setJdbcUrl(env.getProperty("jdbc.url"));
+		securitySource.setUser(env.getProperty("jdbc.user"));
+		securitySource.setPassword(env.getProperty("jdbc.password"));
+		
+		
+		// SET CONNECTION POOL PROPS
+		securitySource.setInitialPoolSize(getIntProp("connection.pool.initialPoolSize"));
+		securitySource.setMinPoolSize(getIntProp("connection.pool.minPoolSize"));
+		securitySource.setMaxPoolSize(getIntProp("connection.pool.maxPoolSize"));
+		securitySource.setMaxIdleTime(getIntProp("connection.pool.maxIdleTime"));
+		
+		
+		return securitySource;
+	}
+	
+	// NEED A HELPER METHOD
+	// READ ENVIRONMENT PROPERTY & CONVERT TO INT
+	private int getIntProp(String prop) {
+		String val = env.getProperty(prop);
+		return Integer.parseInt(val);
 	}
 
 }
--- src/main/java/com/demo/security/config/DemoSecurityConfig.java
+++ src/main/java/com/demo/security/config/DemoSecurityConfig.java
@@ -1,5 +1,8 @@
 package com.demo.security.config;
 
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -12,15 +15,25 @@ import org.springframework.security.core.userdetails.User.UserBuilder;
 @EnableWebSecurity
 public class DemoSecurityConfig extends WebSecurityConfigurerAdapter {
 
+	// ADD REFERNCE TO SECURITY DATASOURCE
+	@Autowired
+	private DataSource securityDataSource;
+	
+	
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 		// ADD USERS
-		UserBuilder users = User.withDefaultPasswordEncoder();
+//		UserBuilder users = User.withDefaultPasswordEncoder();
+//		
+//		auth.inMemoryAuthentication()
+//		.withUser(users.username("john").password("test123").roles("EMPLOYEE"))
+//		.withUser(users.username("mary").password("test123").roles("MANAGER","EMPLOYEE"))
+//		.withUser(users.username("susan").password("test123").roles("ADMIN","EMPLOYEE"));
 		
-		auth.inMemoryAuthentication()
-		.withUser(users.username("john").password("test123").roles("EMPLOYEE"))
-		.withUser(users.username("mary").password("test123").roles("MANAGER","EMPLOYEE"))
-		.withUser(users.username("susan").password("test123").roles("ADMIN","EMPLOYEE"));
+		// JDBC AUTHENTICATION
+		
+		auth.jdbcAuthentication().dataSource(securityDataSource);
+		
 	}
 
 	@Override
